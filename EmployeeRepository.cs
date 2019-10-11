8729bd8e3a5c90a5b1dcdd641777e82ad9cfb25f
Anthony Duque <anthondd@usc.edu>
2019-10-11 15:38:42 -0700

    Initial Commit

---
+++ EmployeeRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LINQSample
+{
+    public class EmployeeRepository
+    {
+        public IEnumerable<Employee> GetAll() {
+            return _employees;
+        }
+
+        List<Employee> _employees = new List<Employee> {
+            new Employee() { ID=1, Name="Anthony", DepartmentID=1 },
+            new Employee() { ID=2, Name="Sheel", DepartmentID=1 },
+            new Employee() { ID=3, Name="Claire", DepartmentID=2 },
+            new Employee() { ID=4, Name="Zheng", DepartmentID=2 },
+            new Employee() { ID=5, Name="Bill", DepartmentID=3 }
+        };
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LINQSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SimpleEmployeeQueries();
+        }
+
+        private static void SimpleEmployeeQueries() {
+            var query1 =
+                from e in new EmployeeRepository().GetAll()
+                where e.DepartmentID < 3 && e.ID < 10
+                orderby e.DepartmentID descending,
+                        e.Name ascending
+                select e;
+            Write(query1);
+
+            var query2 =
+                new EmployeeRepository().GetAll()
+                    .Where(e => e.DepartmentID < 3 && e.ID < 10)
+                    .OrderByDescending(e => e.DepartmentID)
+                    .ThenBy(e => e.Name);
+            Write(query2);
+        }
+
+        static void Write(IEnumerable<Employee> employees) {
+
+            foreach (Employee e in employees) {
+                Console.WriteLine(e.Name);
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
